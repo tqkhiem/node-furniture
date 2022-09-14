90b285105cca81995c007d3c698591f5745fefbe
zombieS12 <93500458+zombies1302@users.noreply.github.com>
2022-09-14 15:19:11 +0700

    Update order.js

--- routes/order.js
+++ routes/order.js
@@ -12,5 +12,14 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.get("/:orderId", async (req, res) => {
+  try {
+    const orders = await Order.find({ _id: req.params.orderId });
+    res.status(200).json(orders);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 
 module.exports = router;
