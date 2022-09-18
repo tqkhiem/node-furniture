c77c53b399e24f918e263f20be2a54ad6b33f9ed
zombieS12 <93500458+zombies1302@users.noreply.github.com>
2022-09-18 13:41:52 +0700

    update jwwt

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,9 @@
 const User = require("../models/User");
 const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+const fs = require('fs');
+const PRIVATE_KEY = fs.readFileSync('./private-key.txt');
+
 
 
 router.post("/", async (req, res) => {
@@ -7,6 +11,27 @@ router.post("/", async (req, res) => {
   try {
     const savedUser = await newUser.save();
     res.status(200).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json("Wrong credentials!");
+    if ( user.password !== req.body.password ){
+      res.status(401).json("Wrong credentials!");
+    }
+    const accessToken = jwt.sign(
+    {
+      id: user._id
+    },
+    PRIVATE_KEY,
+    { expiresIn: "1d" }
+    );
+    res.status(200).json({ username: req.body.username, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }
