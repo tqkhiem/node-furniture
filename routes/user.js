b9e933d0473643db89ce4ae41cdc234cbf7fb28a
zombieS12 <93500458+zombies1302@users.noreply.github.com>
2022-09-18 14:27:10 +0700

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -5,39 +5,69 @@ const fs = require('fs');
 const PRIVATE_KEY = fs.readFileSync('./private-key.txt');
 
 
-
-router.post("/", async (req, res) => {
-  const newUser = new User(req.body);
-  try {
-    const savedUser = await newUser.save();
-    res.status(200).json(savedUser);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.post("/login", async (req, res) => {
-  try {
-
-    const user = await User.findOne({ username: req.body.username });
-    !user && res.status(401).json("Wrong credentials!");
-    if ( user.password !== req.body.password ){
-      res.status(401).json("Wrong credentials!");
+function verifyToken(req, res, next) {
+  const authorizationClient = req.headers['authorization'];
+  const token = authorizationClient && authorizationClient.split(' ')[1]
+  if (!token) return res.sendStatus(401)
+    try {
+      const userInfo = jwt.verify(token, PRIVATE_KEY,(err, user) => {
+        if (err) res.status(403).json("Token is not valid!");
+        res.user = user;
+        next();
+      })
+    } catch (e) {
+      return res.sendStatus(403)
     }
-    const accessToken = jwt.sign(
-    {
-      id: user._id,
-      role: user.role
-    },
-    PRIVATE_KEY,
-    { expiresIn: "1d" }
-    );
-    res.status(200).json({ username: user.username,role: user.role, accessToken });
-  } catch (err) {
-    res.status(500).json(err);
   }
-});
+
+
+  router.post("/", async (req, res) => {
+    const newUser = new User(req.body);
+    try {
+      const savedUser = await newUser.save();
+      res.status(200).json(savedUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  router.post("/login", async (req, res) => {
+    try {
+
+      const user = await User.findOne({ username: req.body.username });
+      !user && res.status(401).json("Wrong credentials!");
+      if ( user.password !== req.body.password ){
+        res.status(401).json("Wrong credentials!");
+      }
+      const accessToken = jwt.sign(
+      {
+        id: user._id,
+        role: user.role
+      },
+      PRIVATE_KEY,
+      { expiresIn: "1d" }
+      );
+      res.status(200).json({ accessToken,role: user.role });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  router.get("/",verifyToken, async (req, res) => {
+    try {
+      const userId = res.user.id;
+      const orders = await User.find({_id:userId});
+      res.status(200).json(orders);
+
+    } catch (err) {
+      res.status(500).json(err);
+    }
 
 
 
-module.exports = router;
+
+
+  });
+
+
+
+  module.exports = router;
