466217c84b5f3e496770b133a52d4cb275519b63
zombieS12 <93500458+zombies1302@users.noreply.github.com>
2022-09-18 16:48:33 +0700

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -8,15 +8,13 @@ const PRIVATE_KEY = fs.readFileSync('./private-key.txt');
 function verifyToken(req, res, next) {
   const authorizationClient = req.headers['authorization'];
   const token = authorizationClient && authorizationClient.split(' ')[1]
-  if (!token) return res.sendStatus(401)
+  if (!token) return res.status(401).json({"message": "Vui lòng nhập Token.","status_code": 401})
     try {
-      const userInfo = jwt.verify(token, PRIVATE_KEY,(err, user) => {
-        if (err) res.status(403).json("Token is not valid!");
-        res.user = user;
-        next();
-      })
+      const userInfo = jwt.verify(token, PRIVATE_KEY)
+      res.user = userInfo
+      next()
     } catch (e) {
-      return res.sendStatus(403)
+      return res.status(403).json({"message": "Token không hợp lệ.","status_code": 403})
     }
   }
 
