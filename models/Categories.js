c1699b371270af335c8e2b006043c20195999716
zombieS12 <93500458+zombies1302@users.noreply.github.com>
2022-10-01 12:36:53 +0700

    Update Categories.js

--- models/Categories.js
+++ models/Categories.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const CategoriesSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    rank: { type: Number, required: true }
+    rank: { type: Number, required: true,unique: true}
   },
   { timestamps: true }
 );
