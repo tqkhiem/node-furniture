e9439e9986c3f66c29e319d61771a339bc953d0d
zombieS12 <93500458+zombies1302@users.noreply.github.com>
2022-10-03 19:42:34 +0700

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -3,15 +3,15 @@ const router = require("express").Router();
 function verifyToken(req, res, next) {
   const authorizationClient = req.headers['authorization'];
   const token = authorizationClient && authorizationClient.split(' ')[1]
-  if (!token) return res.status(401).json({"message": "Vui lòng nhập Token.","status_code": 401})
-    try {
-      const userInfo = jwt.verify(token, PRIVATE_KEY)
-      res.user = userInfo
-      next()
-    } catch (e) {
-      return res.status(403).json({"message": "Token không hợp lệ.","status_code": 403})
-    }
+  if (!token) return res.status(401).json({ "message": "Vui lòng nhập Token.", "status_code": 401 })
+  try {
+    const userInfo = jwt.verify(token, PRIVATE_KEY)
+    res.user = userInfo
+    next()
+  } catch (e) {
+    return res.status(403).json({ "message": "Token không hợp lệ.", "status_code": 403 })
   }
+}
 
 router.get("/", async (req, res) => {
   const sort = req.query.sort;
@@ -41,34 +41,34 @@ router.get("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
-router.post('/',verifyToken, async (req, res) => {
+router.post('/', verifyToken, async (req, res) => {
   const newProduct = new Products(req.body);
   const userRole = res.user.role;
   try {
     if (userRole == "admin") {
       const savedProduct = await newProduct.save();
       res.status(201).json(savedProduct);
-    }else {
-      res.status(403).json({"message": "Bạn không phải là admin hoặc id không đúng.","status_code": 403});  
+    } else {
+      res.status(403).json({ "message": "Bạn không phải là admin hoặc id không đúng.", "status_code": 403 });
     }
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.put('/',verifyToken, async (req, res)=> {
+router.put('/', verifyToken, async (req, res) => {
   const userRole = res.user.role;
   try {
     if (userRole == "admin") {
       const product = await Products.findById(req.body._id);
       if (product) {
         await product.updateOne({ $set: req.body });
-        res.status(200).json({"message": "Đã cập nhật thành công.","status_code": 200});
+        res.status(200).json({ "message": "Đã cập nhật thành công.", "status_code": 200 });
       } else {
-        res.status(404).json({"message": "Không tìm thấy sản phẩm.","status_code": 404});
+        res.status(404).json({ "message": "Không tìm thấy sản phẩm.", "status_code": 404 });
       }
-    }else {
-      res.status(403).json({"message": "Bạn không phải là admin hoặc id không đúng.","status_code": 403});
+    } else {
+      res.status(403).json({ "message": "Bạn không phải là admin hoặc id không đúng.", "status_code": 403 });
     }
   } catch (err) {
     res.status(500).json(err);
